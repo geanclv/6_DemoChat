753aa8dac3a2985d023d8191339a73bf98d88764
geanclv <geanclv@gmail.com>
2022-06-18 15:49:21 -0500

    Initial commit

---
+++ app/src/main/java/com/geancarloleiva/a6_demochat/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.geancarloleiva.a6_demochat.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
