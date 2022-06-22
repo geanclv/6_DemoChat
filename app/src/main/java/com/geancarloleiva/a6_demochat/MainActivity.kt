4605c2cbe3d39b6d77256ce04ca6ec097dfb971e
geanclv <geanclv@gmail.com>
2022-06-22 12:02:09 -0500

    Personalizing the home fragment: text for messages and send messages, chat space; navigation drawer: login and channel buttons, channel space

--- app/src/main/java/com/geancarloleiva/a6_demochat/MainActivity.kt
+++ app/src/main/java/com/geancarloleiva/a6_demochat/MainActivity.kt
@@ -1,8 +1,7 @@
 package com.geancarloleiva.a6_demochat
 
 import android.os.Bundle
-import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
+import android.widget.Button
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -26,10 +25,6 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
-        binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
@@ -42,12 +37,17 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
+        //My code
+        val btnLogin : Button = findViewById(R.id.btnLogin)
+        btnLogin.setOnClickListener{
+
+        }
+        
+        val btnAddChannel : Button = findViewById(R.id.btnAddChannel)
+        btnAddChannel.setOnClickListener{
+
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
