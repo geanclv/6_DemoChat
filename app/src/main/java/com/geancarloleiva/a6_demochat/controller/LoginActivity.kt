031234401e10971df9764d06bd4758c32ca386da
geanclv <geanclv@gmail.com>
2022-07-09 11:35:20 -0500

    Login Activity: Improving IU with progress bar. Auth Service: Adding a GET method with params.

--- app/src/main/java/com/geancarloleiva/a6_demochat/controller/LoginActivity.kt
+++ app/src/main/java/com/geancarloleiva/a6_demochat/controller/LoginActivity.kt
@@ -1,10 +1,14 @@
 package com.geancarloleiva.a6_demochat.controller
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ProgressBar
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.geancarloleiva.a6_demochat.R
 import com.geancarloleiva.a6_demochat.service.AuthService
@@ -13,12 +17,18 @@ import com.geancarloleiva.a6_demochat.util.Utils
 
 class LoginActivity : AppCompatActivity() {
 
+    private lateinit var btnSignin : Button
+    private lateinit var btnLogin: Button
+    private lateinit var progressBarLogin: ProgressBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        progressBarLogin = findViewById(R.id.progressBarLogin)
+
         //Go to create account
-        val btnSignin : Button = findViewById(R.id.btnSignin)
+        btnSignin = findViewById(R.id.btnSignin)
         btnSignin.setOnClickListener{
             val signinIntent = Intent(this, NewAccountActivity::class.java)
             startActivity(signinIntent)
@@ -26,32 +36,54 @@ class LoginActivity : AppCompatActivity() {
         }
 
         //Login
-        val btnLogin: Button = findViewById(R.id.btnLogin)
-        val txtUser: EditText = findViewById(R.id.txtUser)
-        val txtPassword: EditText = findViewById(R.id.txtPassword)
+        btnLogin = findViewById(R.id.btnLogin)
         btnLogin.setOnClickListener{
-            val user = txtUser.text.toString()
-            val password = txtPassword.text.toString()
+            login()
+        }
+    }
 
-            if(user.isNotEmpty() && user.isNotBlank()){
-                if(password.isNotEmpty() && password.isNotBlank()){
-                    AuthService.loginUser(this, user, password){complete ->
-                        if(complete){
-                            Utils.showShortToast(this, "Login OK")
-                            //Broadcasting that the user is Logged in
-                            val userDataChange = Intent(BROADCAST_USER_DATA_CHANGE)
-                            LocalBroadcastManager.getInstance(this).sendBroadcast(userDataChange)
-                            finish()
-                        } else {
-                            Utils.showShortToast(this, "Invalid user or password")
-                        }
+    private fun login(){
+        progressBarVisible(true)
+        hideKeyboard()
+        val user = findViewById<EditText>(R.id.txtUser).text.toString()
+        val password = findViewById<EditText>(R.id.txtPassword).text.toString()
+
+        if(user.isNotEmpty() && user.isNotBlank()){
+            if(password.isNotEmpty() && password.isNotBlank()){
+                AuthService.loginUser(this, user, password){complete ->
+                    if(complete){
+                        Utils.showShortToast(this, "Login OK")
+                        //Broadcasting that the user is Logged in
+                        val userDataChange = Intent(BROADCAST_USER_DATA_CHANGE)
+                        LocalBroadcastManager.getInstance(this).sendBroadcast(userDataChange)
+                        finish()
+                    } else {
+                        Utils.showShortToast(this, "Invalid user or password")
                     }
-                } else{
-                    Utils.showShortToast(this, "Password is required")
                 }
-            } else {
-                Utils.showShortToast(this, "User/Email is required")
+            } else{
+                Utils.showShortToast(this, "Password is required")
             }
+        } else {
+            Utils.showShortToast(this, "User/Email is required")
+        }
+        progressBarVisible(false)
+    }
+
+    private fun progressBarVisible(enable: Boolean){
+        if(enable) {
+            progressBarLogin.visibility = View.VISIBLE
+        } else {
+            progressBarLogin.visibility = View.INVISIBLE
+        }
+        btnSignin.isEnabled = !enable
+        btnLogin.isEnabled = !enable
+    }
+
+    private fun hideKeyboard(){
+        val inputManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        if(inputManager.isAcceptingText){
+            inputManager.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
         }
     }
 }
